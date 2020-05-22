668333195af05e41c9b2ef2622f8f9133b790ea4
skliew <skliew@gmail.com>
2020-05-22 13:05:49 +0800

    Implemented functions in Course.List

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -75,8 +75,8 @@ headOr ::
   a
   -> List a
   -> a
-headOr =
-  error "todo: Course.List#headOr"
+headOr a Nil = a
+headOr _ (x :. _) = x
 
 -- | The product of the elements of a list.
 --
@@ -91,8 +91,8 @@ headOr =
 product ::
   List Int
   -> Int
-product =
-  error "todo: Course.List#product"
+product Nil = 1
+product (x :. xs) = x * (product xs)
 
 -- | Sum the elements of the list.
 --
@@ -106,8 +106,8 @@ product =
 sum ::
   List Int
   -> Int
-sum =
-  error "todo: Course.List#sum"
+sum Nil = 0
+sum (x :. xs) = x + (sum xs)
 
 -- | Return the length of the list.
 --
@@ -118,8 +118,8 @@ sum =
 length ::
   List a
   -> Int
-length =
-  error "todo: Course.List#length"
+length Nil = 0
+length (_ :. xs) = 1 + (length xs)
 
 -- | Map the given function on each element of the list.
 --
@@ -133,8 +133,8 @@ map ::
   (a -> b)
   -> List a
   -> List b
-map =
-  error "todo: Course.List#map"
+map _ Nil = Nil
+map f (x :. xs) = (f x) :. (map f xs)
 
 -- | Return elements satisfying the given predicate.
 --
@@ -150,8 +150,9 @@ filter ::
   (a -> Bool)
   -> List a
   -> List a
-filter =
-  error "todo: Course.List#filter"
+filter _ Nil = Nil
+filter f (x :. xs) | (f x) == True = x :. (filter f xs)
+                   | otherwise = filter f xs
 
 -- | Append two lists to a new list.
 --
@@ -169,8 +170,10 @@ filter =
   List a
   -> List a
   -> List a
-(++) =
-  error "todo: Course.List#(++)"
+(++) Nil Nil = Nil
+(++) Nil l = l
+(++) l Nil = l
+(++) (x :. xs) m = x :. (xs ++ m)
 
 infixr 5 ++
 
@@ -187,8 +190,8 @@ infixr 5 ++
 flatten ::
   List (List a)
   -> List a
-flatten =
-  error "todo: Course.List#flatten"
+flatten Nil = Nil
+flatten (x :. xs) = x ++ (flatten xs)
 
 -- | Map a function then flatten to a list.
 --
@@ -204,8 +207,8 @@ flatMap ::
   (a -> List b)
   -> List a
   -> List b
-flatMap =
-  error "todo: Course.List#flatMap"
+flatMap _ Nil = Nil
+flatMap f (x :. xs) = (f x) ++ (flatMap f xs)
 
 -- | Flatten a list of lists to a list (again).
 -- HOWEVER, this time use the /flatMap/ function that you just wrote.
@@ -214,8 +217,7 @@ flatMap =
 flattenAgain ::
   List (List a)
   -> List a
-flattenAgain =
-  error "todo: Course.List#flattenAgain"
+flattenAgain = flatMap id
 
 -- | Convert a list of optional values to an optional list of values.
 --
@@ -242,8 +244,9 @@ flattenAgain =
 seqOptional ::
   List (Optional a)
   -> Optional (List a)
-seqOptional =
-  error "todo: Course.List#seqOptional"
+seqOptional Nil = Full Nil
+seqOptional (Empty :. _) = Empty
+seqOptional (Full x :. xs) = mapOptional (x :.)  (seqOptional xs)
 
 -- | Find the first element in the list matching the predicate.
 --
@@ -265,8 +268,9 @@ find ::
   (a -> Bool)
   -> List a
   -> Optional a
-find =
-  error "todo: Course.List#find"
+find _ Nil = Empty
+find f (x:.xs) | f x == True = Full x
+               | otherwise = find f xs
 
 -- | Determine if the length of the given list is greater than 4.
 --
@@ -284,8 +288,8 @@ find =
 lengthGT4 ::
   List a
   -> Bool
-lengthGT4 =
-  error "todo: Course.List#lengthGT4"
+lengthGT4 (_ :. _ :. _ :. _ :. _) = True
+lengthGT4 _ = False
 
 -- | Reverse a list.
 --
@@ -301,8 +305,18 @@ lengthGT4 =
 reverse ::
   List a
   -> List a
-reverse =
-  error "todo: Course.List#reverse"
+reverse x = reverseAcc x Nil
+
+reverseAcc :: List a -> List a -> List a
+reverseAcc Nil acc = acc
+reverseAcc (h :. t) acc = let b' = (h :. acc)
+                          in reverseAcc t b'
+
+-- This is sloooww
+-- reverse Nil = Nil
+-- reverse (x :. xs) = let reversed = (reverse xs)
+--                         result = reversed `seq` (reversed ++ (x :. Nil))
+--                     in result `seq` result
 
 -- | Produce an infinite `List` that seeds with the given value at its head,
 -- then runs the given function for subsequent elements
@@ -330,8 +344,13 @@ produce f x = x :. produce f (f x)
 notReverse ::
   List a
   -> List a
-notReverse =
-  error "todo: Is it even possible?"
+notReverse = reverse -- impossible
+
+-- Note:
+-- I missed to look at the helper functions. Could have implemented some of the functions using
+-- those functions.
+-- Also, if we implement `reverse` with `(++)`, it is going to be slow possibly because we're building
+-- up thunks.
 
 ---- End of list exercises
 
